a6df6ef62e6447aec59c60cd5bed7d9c7a2a7d54
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-08-23 23:14:47 -0400

    first commit

---
+++ src/main/java/com/josuejs23/cruddemoterminal/CruddemoterminalApplication.java
@@ -0,0 +1,13 @@
+package com.josuejs23.cruddemoterminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoterminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoterminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josuejs23/cruddemoterminal/CruddemoterminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josuejs23.cruddemoterminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoterminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
